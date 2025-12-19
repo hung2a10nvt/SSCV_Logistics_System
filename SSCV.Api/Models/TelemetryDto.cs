1c1ed3fc3814ef05656ec5d577d29bd64fe381ad
hung2a10nvt <hung2a10@gmail.com>
2025-12-20 02:42:51 +0700

    Typo fixed

--- SSCV.Api/Models/TelemetryDto.cs
+++ SSCV.Api/Models/TelemetryDto.cs
@@ -4,7 +4,7 @@
     {
         public int VehicleId { get; set; }
         public double Longitude { get; set; }
-        public double Latiton { get; set; }
+        public double Latitude { get; set; }
         public decimal Temperature { get; set; }
         public double Speed { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
